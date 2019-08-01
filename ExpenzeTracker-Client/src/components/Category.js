677a15628ed8345e5d8c3e44c6fc64801af3c818
AHMAD <ahmed-rw@outlook.com>
2019-08-01 22:04:10 +0300

    [REFACTOR] moved ranme to header category. applied onBlur

--- ExpenzeTracker-Client/src/components/Category.js
+++ ExpenzeTracker-Client/src/components/Category.js
@@ -1,50 +1,27 @@
 import React, { Component } from 'react'
 import CategoryHeader from './CategoryHeader'
 import { connect } from 'react-redux'
-import { handleRename } from '../store/actions'
 class Category extends Component {
  
     constructor(props) {
         super(props)
-        console.log(props)
-       this.state={
-           newName : "",
-          
-       }
+        this.state = {
+
+        }
+        if(this.props.context === null){
+            this.props.history.push("/NotFound")
+        }
+      
       
     }
 
-    handleNewName = (e) =>{
-        this.setState({
-            newName : e.target.value
-        })
-    }
-  
-
-    handleRename = () =>{
-        const payload={
-            userData : this.props.userData,
-            newName : this.state.newName,
-            categoryId : this.props.context._id
-        }
-        this.props.handleRename(payload, this)
-
-    }
     render() {
-        
-        // if(typeof this.props.location.state ==="undefined"){
-        //     this.props.history.push("/NotFound")
-        // }
-        // const categoryinContext = this.props.location.state.category
-       
         return (
             <React.Fragment>
                 <CategoryHeader />
             {this.state.feedbackMessage}
                 <h1 >this cat's name is {this.props.context.name}</h1>
-                <label>Rename category</label>
-                <input type="text" id="categoryName" onChange={this.handleNewName} placeholder="new category name" />
-                <button onClick={this.handleRename} >Rename</button>
+
             </React.Fragment>
         )
     }
@@ -59,7 +36,6 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        handleRename : (payload,Component) =>{dispatch(handleRename(payload, Component))}
     }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(Category);
--- ExpenzeTracker-Client/src/components/CategoryHeader.js
+++ ExpenzeTracker-Client/src/components/CategoryHeader.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react'
 
-
+import { connect } from 'react-redux'
+import { handleRename } from '../store/actions'
 class CategoryHeader extends Component {
+
+
+
+   state={
+        newName : this.props.context.newName,
+       
+    }
+    handleNewName = (e) =>{
+        this.setState({
+            newName : e.target.value
+        })
+    }
+  
+
+    handleRename = () =>{
+        const payload={
+            userData : this.props.userData,
+            newName : this.state.newName,
+            categoryId : this.props.context._id
+        }
+        this.props.handleRename(payload, this)
+
+    }
+
     render() {
         return (
             <React.Fragment>
+            <label>Rename category</label>
+                <input type="text" id="categoryName" onBlur={this.handleRename} onChange={this.handleNewName} placeholder="new category name" />
+                <button onClick={this.handleRename} >Rename</button>
                 <h1>This is a component for a header of a category</h1>
             </React.Fragment>
         )
@@ -12,4 +40,18 @@ class CategoryHeader extends Component {
 }
 
 
-export default CategoryHeader
+
+const mapStateToProps = state => {
+    return {
+        userData: state.userData,
+        context : state.context
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        handleRename : (payload,Component) =>{dispatch(handleRename(payload, Component))}
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryHeader);
+
