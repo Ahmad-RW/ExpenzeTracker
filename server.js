e20c532f7fcf332db5dc063e8e1e1511509f375a
AHMAD <ahmed-rw@outlook.com>
2019-09-04 22:33:44 +0300

    takeoff 3

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ const account = require('./account')
 const getUserDto = require('./dbConfig/models/UserDto')
 const path = require('path')
 const port = process.env.PORT || 5000
+const cors = require('cors')
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 
 });
-
+app.use(cors())
 app.use(bodyParser.json());// post request body parser
 app.use("/account", account)
 
